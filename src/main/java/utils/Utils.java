3c7b4ef58f4c9f6273a31cfa4fe569906739c450
Pawel Kozubala <p.kozubala@gmail.com>
2021-03-27 08:03:01 +0100

    add organization model

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Utils {
+
+    public static final String HTTPS_PREFIX = "https://";
+    public static final String ERROR_MESSAGE_DISPLAY_NAME = "Display Name must be at least 1 character";
+    public static final String ERROR = "ERROR";
+}
--- src/test/java/organization/CreateNewOrganizationTest.java
+++ src/test/java/organization/CreateNewOrganizationTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.Utils;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,21 +15,21 @@ public class CreateNewOrganizationTest extends BaseTest {
 
     @BeforeEach
     public void beforeEach() {
-        organization.setDisplayName(faker.lorem().sentence());
+        organization.setDisplayName(faker.lorem().word());
         organization.setDescription(faker.lorem().paragraph());
-        organization.setName(faker.lorem().sentence());
+        organization.setName(faker.lorem().characters(4, 10, false, false));
         organization.setWebsite(faker.internet().url());
     }
 
     @Test
-    public void createNewOrganizationWithAllAvailableFileds(){
+    public void createNewOrganizationWithAllAvailableFileds() {
 
         Response response = given()
                 .spec(reqSpec)
                 .queryParam("displayName", organization.getDisplayName())
                 .queryParam("desc", organization.getDescription())
                 .queryParam("name", organization.getName())
-                .queryParam("website", organization.getWebsite())
+                .queryParam("website", Utils.HTTPS_PREFIX + organization.getWebsite())
                 .when()
                 .post(BASE_URL + ORGANIZATION)
                 .then()
@@ -40,6 +41,48 @@ public class CreateNewOrganizationTest extends BaseTest {
         assertThat(json.getString("displayName")).isEqualTo(organization.getDisplayName());
         assertThat(json.getString("desc")).isEqualTo(organization.getDescription());
         assertThat(json.getString("name")).isEqualTo(organization.getName());
-        assertThat(json.getString("website")).isEqualTo(organization.getWebsite());
+        assertThat(json.getString("website")).isEqualTo(Utils.HTTPS_PREFIX + organization.getWebsite());
     }
+
+    @Test
+    public void createNewOrganizationWithoutDisplayName() {
+
+        Response response = given()
+                .spec(reqSpec)
+                .queryParam("desc", organization.getDescription())
+                .queryParam("name", organization.getName())
+                .queryParam("website", Utils.HTTPS_PREFIX + organization.getWebsite())
+                .when()
+                .post(BASE_URL + ORGANIZATION)
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        assertThat(json.getString("error")).isEqualTo(Utils.ERROR);
+        assertThat(json.getString("message")).isEqualTo(Utils.ERROR_MESSAGE_DISPLAY_NAME);
+//        assertThat(json).doesNotHaveJsonPathValue("")
+    }
+
+    @Test
+    public void createNewOrganizationOnlyWithDisplayNameFiled() {
+
+        Response response = given()
+                .spec(reqSpec)
+                .queryParam("displayName", organization.getDisplayName())
+                .when()
+                .post(BASE_URL + ORGANIZATION)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        assertThat(json.getString("displayName")).isEqualTo(organization.getDisplayName());
+        assertThat(json.getString("desc")).isEmpty();
+        assertThat(json.getString("name")).isNotNull();
+        assertThat(json.getString("website")).isNull();
+    }
+
 }
